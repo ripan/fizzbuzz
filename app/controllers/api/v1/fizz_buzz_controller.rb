9b7bba0a9e16d777c04e44466f826f11f30989ad
Ripan <er.ripankumar@gmail.com>
2016-05-22 21:17:27 +0100

    Add fizz buzz api

--- app/controllers/api/v1/fizz_buzz_controller.rb
+++ app/controllers/api/v1/fizz_buzz_controller.rb
@@ -1,9 +1,8 @@
-class HomeController < ApplicationController
+class Api::V1::FizzBuzzController < ApplicationController
 
   def index
     fizzBuzz = FizzBuzz.new
     fizzBuzz_range = Kaminari.paginate_array(fizzBuzz.range).page(params[:page]).per(params[:page_size])
-
     render  json: fizzBuzz_range
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  
+  namespace :api do
+    namespace :v1 do
+      get "fizzbuzz/list" =>  'fizz_buzz#index'
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'api/v1/fizz_buzz#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
