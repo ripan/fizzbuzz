a0cfbad1dbdfe5e905f008ba7e123f56043fc6da
Ripan <er.ripankumar@gmail.com>
2016-05-23 00:20:03 +0100

    Fix Access-Control-Allow-Origin

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,11 @@ module Fizzbuzz
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.autoload_paths << Rails.root.join('lib')
+
+    config.action_dispatch.default_headers.merge!({
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => '*'
+    })
+    
   end
 end
