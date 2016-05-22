23f1fe8c63d31a1fab80697240ee26dc5f4a99a4
Ripan <er.ripankumar@gmail.com>
2016-05-22 18:46:45 +0100

    Initialise fizzbuzz API
    rails-api new fizzbuzz --skip-active-record --skip-sprockets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Fizzbuzz::Application.config.secret_token = '752516322d75575ace69a140a21bb774e90c80b45a622268ce442b1113a912e6653c0105fdd2ea0c77203d58bdc690a8b1c98a847cc6d205ed7f8ce2dc575daa'
