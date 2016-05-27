daad7940754293299a9cd748ab41bc57a84b0d27
Ripan <er.ripankumar@gmail.com>
2016-05-27 14:49:52 +0100

    Change index to id

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -13,7 +13,7 @@ class FizzBuzz
       tmp = ''
       tmp += 'Fizz' if i%3==0
       tmp += 'Buzz' if i%5==0
-      {index:i, name:tmp.empty? ? i : tmp, isFavourite:false}
+      {id:i, name:tmp.empty? ? i : tmp, isFavourite:false}
     end
   end
 
